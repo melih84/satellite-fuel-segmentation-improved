3dc1cd4dbcd8249f2b8e34e218756008f179d25f
Maziar Jamshidi <maziar.jamshidi@ucalgary.ca>
2024-03-14 19:57:40 -0600

    Save metrics in a dictionary

--- src/evaluate.py
+++ src/evaluate.py
@@ -9,15 +9,17 @@ def make_predicitons(model, X):
     y_pred_one_hot = np.moveaxis(y_pred_one_hot, 0, -1) 
     return probs, y_pred_one_hot
 
-def get_metrics(ground_truth, prediction):
+def get_metrics(ground_truth, prediction, class_to_fuel=None):
     n_class = ground_truth.shape[-1]
-    pr, rc, f1, iou, px = [], [], [], [], []
+    pr, rc, f1, iou, ratio = {}, {}, {}, {}, {}
     for i in range(n_class):
+        cls_id = (i if class_to_fuel is None else class_to_fuel[i])
         y_true = ground_truth[:,:,:,i].flatten()
         y_pred = prediction[:,:,:,i].flatten()
-        pr.append(precision_score(y_true, y_pred))
-        rc.append(recall_score(y_true, y_pred))
-        f1.append(f1_score(y_true, y_pred))
-        iou.append(jaccard_score(y_true, y_pred))
-        px.append(y_true.sum())
-    return pr, rc, f1, iou, px
+        pr[cls_id] = precision_score(y_true, y_pred)
+        rc[cls_id] = recall_score(y_true, y_pred)
+        f1[cls_id] = f1_score(y_true, y_pred)
+        iou[cls_id] = jaccard_score(y_true, y_pred)
+        ratio[cls_id] = y_true.sum() / y_true.size
+    return pr, rc, f1, iou, ratio
+
