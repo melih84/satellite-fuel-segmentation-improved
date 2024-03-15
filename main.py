735aa33dfabba9b8ba778760bdc7e81c84269bd6
Maziar Jamshidi <maziar.jamshidi@ucalgary.ca>
2024-03-15 17:15:36 -0600

    Save sample images

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import random
+import os
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -13,14 +15,15 @@ from src.utils import Visualize
 IMAGE_SIZE = (128, 128)
 
 class_info = "data/class_info.csv"
+df = pd.read_csv(class_info)
+df.index = df["name"]
+class_dict= df[["r", "g", "b"]].transpose().to_dict()
+
+class_to_fuel = {i:cls for i, cls in enumerate(class_dict.keys())}
+fuel_to_class = {cls:i for i, cls in enumerate(class_dict.keys())}
 
 def main():
-    df = pd.read_csv(class_info)
-    df.index = df["name"]
-    class_dict= df[["r", "g", "b"]].transpose().to_dict()
-
     train_root = "data/training_dataset/"
-
     train_data = Data(image_dir=train_root + "images/",
                     mask_dir=train_root + "masks/",
                     class_dict=class_dict)
@@ -49,7 +52,7 @@ def main():
 
     hist = model.fit(X_train, y_train,
                     batch_size=2,
-                    epochs=3,
+                    epochs=1,
                     validation_data=(X_valid, y_valid),
                     callbacks=[tb_cb, es_cb])
 
@@ -64,9 +67,31 @@ def main():
         json.dump(metrics, f)
 
     print(pd.DataFrame(metrics))
-    # probs = model.predict(X_valid)
+    #TODO add checkpoints
 
-    # y_pred = np.argmax(probs, axis=-1)
+    n = min(X_valid.shape[0], 10)
+    ix = list(range(X_valid.shape[0]))
+    random.shuffle(ix)
+    save_samples(X_valid, y_valid, y_pred, ix[:n])
+    
+
+def save_samples(X, y_true, y_pred, sample_list):
+    fuel_color = {
+        "SurfaceFuels": "palegreen",
+        "Canopy": "darkgreen",
+        "Void": "gray"}
+    color_scheme = [fuel_color[f] for _, f in class_to_fuel.items()]
+    viz = Visualize(color_scheme)
+    save_dir = os.path.join(root_path, study_id, "samples/")
+    os.makedirs(save_dir) if not os.path.exists(save_dir) else None
+    for m in sample_list:
+        viz.display(X[m,:,:,:],
+                    y_true[m,:,:,:],
+                    y_pred[m,:,:,:],
+                    save_to=save_dir+f"{m}.png")
+
+    print()
+    print(f"Sample images saved to {save_dir}")
 
 
 if __name__ == "__main__":
