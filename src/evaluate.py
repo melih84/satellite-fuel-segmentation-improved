cfb11f127aea13926616265dcf07c98e49e2e328
Maziar Jamshidi <maziar.jamshidi@ucalgary.ca>
2024-03-14 19:32:32 -0600

    Add metrics

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import precision_score, recall_score, f1_score, jaccard_score
 
 def make_predicitons(model, X):
     probs = model.predict(X)
@@ -7,3 +8,15 @@ def make_predicitons(model, X):
     y_pred_one_hot = np.array([(y_pred == i).astype("int") for i in range(n_class)])
     y_pred_one_hot = np.moveaxis(y_pred_one_hot, 0, -1) 
     return probs, y_pred_one_hot
+
+def get_metrics(ground_truth, prediction):
+    n_class = ground_truth.shape[-1]
+    pr, rc, f1, iou = [], [], [], []
+    for i in range(n_class):
+        y_true = ground_truth[:,:,:,i].flatten()
+        y_pred = prediction[:,:,:,i].flatten()
+        pr.append(precision_score(y_true, y_pred))
+        rc.append(recall_score(y_true, y_pred))
+        f1.append(f1_score(y_true, y_pred))
+        iou.append(jaccard_score(y_true, y_pred))
+    return pr, rc, f1, iou
