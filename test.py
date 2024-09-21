fe68e960a6adee5f744a724200da1b6c81acaac0
Maziar Jamshidi <mz.jamshidi@gmail.com>
2024-09-20 18:31:21 -0600

    save predicted masks

--- test.py
+++ test.py
@@ -1,28 +1,40 @@
 import argparse
+from pathlib import Path
 
 import yaml
 from tensorflow.keras.models import load_model
+import cv2
 
 from src.data import Dataset, DataGenerator
-from src.utils import probs_to_one_hot
+from src.utils import probs_to_one_hot, one_hot_to_rgb, increment_path
 
 
 def test(data_dict,
          img_size,
          batch_size,
          model,
-         task):
+         task,
+         save_dir):
     
     path = data_dict[task]
     dataset = Dataset(path=path,
                       img_size=img_size,
                       shuffle=False)
+    
     test_data = DataGenerator(dataset=dataset,
+                              task="test",
                               batch_size=batch_size)
     
     probs = model.predict(test_data)
-    outputs = probs_to_one_hot(probs)
-    breakpoint()
+    one_hots = probs_to_one_hot(probs)
+    pred_masks = one_hot_to_rgb(one_hots, color_ids=data_dict["color_ids"])
+    save_image(pred_masks, test_data.ids, save_dir)
+
+def save_image(images, ids, save_dir=""):
+    for image, id in zip(images, ids):
+        path = Path(save_dir) / f"{id}.png"
+        cv2.imwrite(path, image)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -31,10 +43,16 @@ if __name__ == "__main__":
     parser.add_argument("--batch-size", type=int, default=1)
     parser.add_argument("--image-size", type=int, default=320)
     parser.add_argument("--task", default="test")
+    parser.add_argument('--project', default='runs/test', help='save to project/name')
+    parser.add_argument('--name', default='exp', help='save to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
 
     opt = parser.parse_args()
-    print(opt)
+    opt.save_dir = increment_path(Path(opt.project) / opt.name,
+                                  exist_ok=opt.exist_ok,
+                                  sep="_")  # increment run
 
+    Path(opt.save_dir).mkdir(parents=True, exist_ok=True)
     model = load_model(opt.model)
     with open(opt.data, "r") as f:
         data_dict = yaml.load(f, Loader=yaml.SafeLoader)
@@ -43,4 +61,5 @@ if __name__ == "__main__":
          img_size=opt.image_size,
          batch_size=opt.batch_size,
          model=model,
-         task=opt.task)
+         task=opt.task,
+         save_dir=opt.save_dir)
