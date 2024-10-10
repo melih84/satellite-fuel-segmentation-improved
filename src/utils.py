dd20a65d581dbc3e4f553a0ebe7f0db0c52746a2
Maziar Jamshidi <mz.jamshidi@gmail.com>
2024-10-10 11:13:55 -0600

    fix reg exp typo for save dir

--- src/utils.py
+++ src/utils.py
@@ -140,7 +140,7 @@ def increment_path(path, exist_ok=True, sep=''):
         return str(path)
     else:
         dirs = glob.glob(f"{path}{sep}*")  # similar paths
-        matches = [re.search(rf"%s{sep}(/d+)" % path.stem, d) for d in dirs]
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
         i = [int(m.groups()[0]) for m in matches if m]  # indices
         n = max(i) + 1 if i else 2  # increment number
         return f"{path}{sep}{n}"  # update path
